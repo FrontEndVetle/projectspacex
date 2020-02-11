e1cc213d8e85f303e94e3484196647237a157a44
Eilert Vetle Forland <ev.forland@gmail.com>
2020-02-11 08:36:55 +0100

    fetched latest launch api to index page and display date and time in inner.html.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let nextLaunch;
+let latestLaunch;
 
 
 fetch("https://api.spacexdata.com/v3/launches/next")
@@ -18,9 +19,37 @@ fetch("https://api.spacexdata.com/v3/launches/next")
 
 function nextLaunchIs() {
     document.getElementById("nextLaunch").innerHTML =
-        "<p>THE NEXT LAUNCH IS: <br>" +
+        "<p>THE NEXT SPACE<B>X</B> LAUNCH IS: <br>" +
         nextLaunch.mission_name +
         " " +
         nextLaunch.launch_date_local +
+        " </p> ";
+};
+
+
+
+
+
+fetch("https://api.spacexdata.com/v3/launches/latest")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        let data = json;
+        latestLaunch = data;
+        latestLaunchIs(json);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+
+
+
+function latestLaunchIs() {
+    document.getElementById("latestLaunch").innerHTML =
+        "<p>MOST RECENT SPACE<B>X</B> LAUNCH IS: <br>" +
+        latestLaunch.mission_name +
+        " " +
+        latestLaunch.launch_date_local +
         " </p> ";
 };
