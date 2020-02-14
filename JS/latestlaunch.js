118e9845753805095dca9c164eaeac28b623e046
Eilert Vetle Forland <ev.forland@gmail.com>
2020-02-14 15:17:36 +0100

    added filter function for selected mission in latestlaunch.js

--- JS/latestlaunch.js
+++ JS/latestlaunch.js
@@ -16,8 +16,26 @@ function loopThroughLaunched(launched) {
     let missionimg = document.getElementById("gallerySlides");
     for (var i = 0; i < 3; i++) {
         missionimg.innerHTML +=
-            "<img src = '" +
+            "<article class='mission'><div class='caption'><bar> " +
+            launched[i].mission_name[0] +
+            "</bar></div><img src = '" +
             launched[i].links.flickr_images[0] +
-            "' class='gallery-image' alt='image of mission rocket'>";
+            "' class='gallery-image' alt='image of mission rocket'>" +
+            launched[i].launch_year[0] +
+            "  </article>";
     }
-};
+    document.querySelectorAll(".mission").forEach(item => {
+        item.addEventListener("click", function() {
+            var filteredLaunch = launched.filter(
+                launch => launch.links.flickr_images[0] === item.firstChild.src
+            );
+            var hideFilter = document.querySelector(".slider");
+            let missionInfo = document.querySelector(".selected-mission")
+            hideFilter.innerHTML = "";
+            missionInfo.innerHTML =
+                "<div><p>" +
+                filteredLaunch[0].mission_name +
+                "</p></div>";
+        });
+    });
+}
